dee70fbbadd48a5766f8b87ad6b1bce267747e6c
Cesar Ibarra <cesarij07@gmail.com>
2024-10-31 13:04:28 -0600

    feat(express): actividad 2

---
+++ config/routes.config.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../config/multer.config')
+const users = require('../controllers/users.controller')
+const posts = require('../controllers/posts.controller')
+const sec = require('../middlewares/secure.middleware')
+
+
+router.get('/posts', posts.list)
+router.post('/posts', posts.create)
+router.get('/posts/:id', posts.info)
+router.put('/posts/:id', posts.update)
+router.delete('/posts/:id', posts.delete)
+// router.post('/posts/:id/like', sec.auth, posts.like)
+// router.post('/posts/:id/comments', sec.auth, posts.createComment)
+// router.delete('/posts/:id/comments/:commentId', sec.auth, sec.commentOwner, posts.deleteComment)
+// router.post('/users', upload.single('avatar'), users.create)
+// router.get('/users/:id', sec.auth, users.get)
+// router.patch('/users/:id', sec.auth, sec.self, upload.single('avatar'), users.update)
+// router.delete('/users/:id', sec.auth, sec.self, users.delete)
+
+// router.post('/login', users.login)
+// router.post('/logout', sec.auth, users.logout)
+// router.get('/authenticate/google', users.loginWithGoogle)
+// router.get('/authenticate/google/cb', users.doLoginWithGoogle)
+
+module.exports = router;
---
+++ controllers/posts.controller.js
@@ -0,0 +1,103 @@
+const createError = require('http-errors');
+const Like = require('../models/like.model');
+const Comment = require('../models/comment.model');
+const Post = require('../models/post.model');
+
+module.exports.list = (req, res, next) => {
+  const criteria = {}
+
+  if (req.query.search) {
+    criteria.post = new RegExp(req.query.search, 'i')
+  }
+
+  Post.find(criteria)
+    .then(posts => res.json(posts))
+    .catch(next);
+    // .populate('likes')
+    // .populate({
+    //   path: 'comments',
+    //   populate: {
+    //     path: 'user'
+    //   } 
+    // })
+    // .populate('author')
+
+}
+
+module.exports.info = (req, res, next) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.json(post)
+      } else {
+        next(createError(404, 'post not found'))
+      }
+    })
+    .catch(next)
+}
+
+module.exports.delete = async (req, res, next) => {
+  try {
+    const postId = req.params.id; // Assuming the post ID is provided in the URL params
+    const deletedPost = await Post.findByIdAndDelete(postId); // Find and delete the post by ID
+
+    if (!deletedPost) {
+      return res.status(404).json({ message: "Post not found" }); // Handle case where post does not exist
+    }
+
+    res.status(204).send(); // Successfully deleted, send no content response
+  } catch (error) {
+    next(error); // Pass any errors to the error handling middleware
+  }
+};
+
+module.exports.create = (req, res, next) => {
+  const data = { text } = req.body
+
+  Post.create({
+    ...data,
+  })
+    .then(post => res.status(201).json(post))
+    .catch(next)
+}
+
+module.exports.update = (req, res, next) => {
+  const data = req.body;
+
+  Post.findOneAndUpdate({ _id: req.params.id }, data, { new: true })
+    .then((updatedPost) => {
+      if (!updatedPost) {
+        return res.status(404).json({ message: "Post not found" });
+      }
+      res.json(updatedPost);
+    })
+    .catch(next);
+};
+
+
+module.exports.like = (req, res, next) => {
+  const data = { post: req.params.id, user: req.user.id }
+
+  Like.findOne(data)
+    .then(like => like ? like.delete() : Like.create(data))
+    .then(() => res.status(204).send())
+    .catch(next)
+}
+
+module.exports.createComment = (req, res, next) => {
+  const data = { text } = req.body
+
+  Comment.create({
+    ...data,
+    user: req.user.id,
+    post: req.params.id
+  })
+    .then((comment) => res.status(201).send(comment))
+    .catch(next)
+}
+
+module.exports.deleteComment = (req, res, next) => {
+  req.comment.delete()
+    .then(() => res.status(204).send())
+    .catch(next)
+}
